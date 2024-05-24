deb2815d4c5330df2a72f0a91284051e666fe321
marialejandravc <159529153+marialejandravc@users.noreply.github.com>
2024-05-24 09:39:04 +0000

    add functions to generate domain

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  function generateDomains() {
+    let pronoun = ["my", "your"];
+    let adj = ["amazing", "huge"];
+    let noun = ["cat", "dog"];
+    let extensions = [".com", ".net", ".us", ".io"];
+    let hacks = { es: "walk", it: "eat", in: "write" };
+
+    let resultsDiv = document.getElementById("domainResults");
+    resultsDiv.innerHTML = "";
+
+    // Generar dominios normales
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < extensions.length; l++) {
+            let domain = `${pronoun[i]}${adj[j]}${noun[k]}${extensions[l]}`;
+            resultsDiv.innerHTML += `<p>${domain}</p>`;
+          }
+        }
+      }
+    }
+
+    // Generar "domain hacks"
+    for (let ext in hacks) {
+      for (let i = 0; i < pronoun.length; i++) {
+        for (let j = 0; j < adj.length; j++) {
+          let domain = `${pronoun[i]}${adj[j]}${hacks[ext]}.${ext}`;
+          resultsDiv.innerHTML += `<p>${domain}</p>`;
+        }
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
+  window.generateDomains = generateDomains;
 };
